956d75e2445e864862f88023f8754d77e92d7675
KethelynAlves <141461344+KethelynAlves@users.noreply.github.com>
2023-08-15 19:22:17 -0300

    Primeira versão do prg de Cálculo da Hipotenusa
    Programa em python que calcula  a hipotenusa por meio dos catetos digitados pelo usuário.

---
+++ hipotenusa.py
@@ -0,0 +1,6 @@
+import math
+cat1 = float (input("Digite o primeiro cateto: "))
+cat2 = float (input("Digite o segundo  cateto: "))
+hipo = (cat1**2 + cat2**2)
+hipotenusa = math.sqrt(hipo)
+print ("A hipotenusa é",hipotenusa)
